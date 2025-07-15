a9f47989616a2b21c44d1f284af7674528973941
JakeLDev <me@jakelyell.dev>
2025-07-15 17:17:42 +1000

    Update vite config to fix gh-pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Portfolio-Website-v2/',
   resolve: {
     alias: [
       { find: '@', replacement: '/src' }
